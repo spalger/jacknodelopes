f7b1e427cbd03fe2a09edf50bd5a345dc1f26275
Spencer Alger <spencer@spenceralger.com>
2013-02-24 16:19:05 -0700

    added simple 'board'

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+
+var board = [
+	[0,0,0,0,0,0],
+	[0,0,0,0,0,0],
+	[0,0,0,0,0,0],
+	[0,0,0,0,0,0],
+	[0,0,0,0,0,0],
+	[0,0,0,0,0,0]
+];
+
 function drawBoard() {
-	
+
+	board.forEach(function(row, cols) {
+		var html = '<tr>';
+		cols.forEach(function(col, player) {
+			html+= '<td class="player-'+player+'"></td>';
+		})
+		$row
+	})
 }
