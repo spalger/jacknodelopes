6bc8b01586a60864d36834414d3877107e767275
redaphid <aherres@gmail.com>
2013-03-09 13:51:22 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-
-var board = [
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0]
-];
-
 function drawBoard() {
-
-	board.forEach(function(row, cols) {
-		var html = '<tr>';
-		cols.forEach(function(col, player) {
-			html+= '<td class="player-'+player+'"></td>';
-		})
-		$row
-	})
+	
 }
